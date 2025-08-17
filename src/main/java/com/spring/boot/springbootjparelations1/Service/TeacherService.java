5468473eb3dfcc0394cb52b014ea8e55f15a7e10
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-08-17 09:57:38 +0300

    to be continued ...

--- src/main/java/com/spring/boot/springbootjparelations1/Service/TeacherService.java
+++ src/main/java/com/spring/boot/springbootjparelations1/Service/TeacherService.java
@@ -1,12 +1,22 @@
 package com.spring.boot.springbootjparelations1.Service;
 
+import com.spring.boot.springbootjparelations1.Model.Teacher;
 import com.spring.boot.springbootjparelations1.Repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TeacherService {
     private final TeacherRepository teacherRepository;
 
+    public List<Teacher> getAllTeachers(){
+        return teacherRepository.findAll();
+    }
+
+    public void addNewTeacher(Teacher teacher){
+        teacherRepository.save(teacher);
+    }
 }
